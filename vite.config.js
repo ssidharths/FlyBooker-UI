4b6712c8a465b3ae1f30676016232932550ec711
Sidharth <sidharths550@gmail.com>
2025-09-13 15:59:16 +0530

    add: netlify toml file

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
-      '/fb': {
-        target: 'http://13.201.104.217:8081',
+      "/api": {
+        target: "http://13.201.104.217:8081/fb/api/v1",
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
